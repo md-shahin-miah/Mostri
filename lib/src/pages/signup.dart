7b703257816d7c66865f6e80bcfe5fba8e7a85f5
shahintahsan225@gmail.com <shahin550@@##&&>
2023-01-07 12:17:58 +0600

    Initial commit

--- lib/src/pages/signup.dart
+++ lib/src/pages/signup.dart
@@ -18,6 +18,7 @@ class _SignUpWidgetState extends StateMVC<SignUpWidget> {
   _SignUpWidgetState() : super(UserController()) {
     _con = controller;
   }
+
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
@@ -43,19 +44,25 @@ class _SignUpWidgetState extends StateMVC<SignUpWidget> {
                 height: config.App(context).appHeight(29.5),
                 child: Text(
                   S.of(context).lets_start_with_register,
-                  style: Theme.of(context).textTheme.headline2.merge(TextStyle(color: Theme.of(context).primaryColor)),
+                  style: Theme.of(context)
+                      .textTheme
+                      .headline2
+                      .merge(TextStyle(color: Theme.of(context).primaryColor)),
                 ),
               ),
             ),
             Positioned(
               top: config.App(context).appHeight(29.5) - 50,
               child: Container(
-                decoration: BoxDecoration(color: Theme.of(context).primaryColor, borderRadius: BorderRadius.all(Radius.circular(10)), boxShadow: [
-                  BoxShadow(
-                    blurRadius: 50,
-                    color: Theme.of(context).hintColor.withOpacity(0.2),
-                  )
-                ]),
+                decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 50,
+                        color: Theme.of(context).hintColor.withOpacity(0.2),
+                      )
+                    ]),
                 margin: EdgeInsets.symmetric(
                   horizontal: 20,
                 ),
@@ -71,48 +78,93 @@ class _SignUpWidgetState extends StateMVC<SignUpWidget> {
                       TextFormField(
                         keyboardType: TextInputType.text,
                         onSaved: (input) => _con.user.name = input,
-                        validator: (input) => input.length < 3 ? S.of(context).should_be_more_than_3_letters : null,
+                        validator: (input) => input.length < 3
+                            ? S.of(context).should_be_more_than_3_letters
+                            : null,
                         decoration: InputDecoration(
                           labelText: S.of(context).full_name,
-                          labelStyle: TextStyle(color: Theme.of(context).accentColor),
+                          labelStyle:
+                              TextStyle(color: Theme.of(context).accentColor),
                           contentPadding: EdgeInsets.all(12),
                           hintText: S.of(context).john_doe,
-                          hintStyle: TextStyle(color: Theme.of(context).focusColor.withOpacity(0.7)),
-                          prefixIcon: Icon(Icons.person_outline, color: Theme.of(context).accentColor),
-                          border: OutlineInputBorder(borderSide: BorderSide(color: Theme.of(context).focusColor.withOpacity(0.2))),
-                          focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Theme.of(context).focusColor.withOpacity(0.5))),
-                          enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Theme.of(context).focusColor.withOpacity(0.2))),
+                          hintStyle: TextStyle(
+                              color: Theme.of(context)
+                                  .focusColor
+                                  .withOpacity(0.7)),
+                          prefixIcon: Icon(Icons.person_outline,
+                              color: Theme.of(context).accentColor),
+                          border: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: Theme.of(context)
+                                      .focusColor
+                                      .withOpacity(0.2))),
+                          focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: Theme.of(context)
+                                      .focusColor
+                                      .withOpacity(0.5))),
+                          enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: Theme.of(context)
+                                      .focusColor
+                                      .withOpacity(0.2))),
                         ),
                       ),
                       SizedBox(height: 30),
                       TextFormField(
                         keyboardType: TextInputType.emailAddress,
                         onSaved: (input) => _con.user.email = input,
-                        validator: (input) => !input.contains('@') ? S.of(context).should_be_a_valid_email : null,
+                        validator: (input) => !input.contains('@')
+                            ? S.of(context).should_be_a_valid_email
+                            : null,
                         decoration: InputDecoration(
                           labelText: S.of(context).email,
-                          labelStyle: TextStyle(color: Theme.of(context).accentColor),
+                          labelStyle:
+                              TextStyle(color: Theme.of(context).accentColor),
                           contentPadding: EdgeInsets.all(12),
                           hintText: 'johndoe@gmail.com',
-                          hintStyle: TextStyle(color: Theme.of(context).focusColor.withOpacity(0.7)),
-                          prefixIcon: Icon(Icons.alternate_email, color: Theme.of(context).accentColor),
-                          border: OutlineInputBorder(borderSide: BorderSide(color: Theme.of(context).focusColor.withOpacity(0.2))),
-                          focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Theme.of(context).focusColor.withOpacity(0.5))),
-                          enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Theme.of(context).focusColor.withOpacity(0.2))),
+                          hintStyle: TextStyle(
+                              color: Theme.of(context)
+                                  .focusColor
+                                  .withOpacity(0.7)),
+                          prefixIcon: Icon(Icons.alternate_email,
+                              color: Theme.of(context).accentColor),
+                          border: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: Theme.of(context)
+                                      .focusColor
+                                      .withOpacity(0.2))),
+                          focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: Theme.of(context)
+                                      .focusColor
+                                      .withOpacity(0.5))),
+                          enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: Theme.of(context)
+                                      .focusColor
+                                      .withOpacity(0.2))),
                         ),
                       ),
                       SizedBox(height: 30),
                       TextFormField(
                         obscureText: _con.hidePassword,
                         onSaved: (input) => _con.user.password = input,
-                        validator: (input) => input.length < 6 ? S.of(context).should_be_more_than_6_letters : null,
+                        validator: (input) => input.length < 6
+                            ? S.of(context).should_be_more_than_6_letters
+                            : null,
                         decoration: InputDecoration(
                           labelText: S.of(context).password,
-                          labelStyle: TextStyle(color: Theme.of(context).accentColor),
+                          labelStyle:
+                              TextStyle(color: Theme.of(context).accentColor),
                           contentPadding: EdgeInsets.all(12),
                           hintText: '••••••••••••',
-                          hintStyle: TextStyle(color: Theme.of(context).focusColor.withOpacity(0.7)),
-                          prefixIcon: Icon(Icons.lock_outline, color: Theme.of(context).accentColor),
+                          hintStyle: TextStyle(
+                              color: Theme.of(context)
+                                  .focusColor
+                                  .withOpacity(0.7)),
+                          prefixIcon: Icon(Icons.lock_outline,
+                              color: Theme.of(context).accentColor),
                           suffixIcon: IconButton(
                             onPressed: () {
                               setState(() {
@@ -120,18 +172,33 @@ class _SignUpWidgetState extends StateMVC<SignUpWidget> {
                               });
                             },
                             color: Theme.of(context).focusColor,
-                            icon: Icon(_con.hidePassword ? Icons.visibility : Icons.visibility_off),
+                            icon: Icon(_con.hidePassword
+                                ? Icons.visibility
+                                : Icons.visibility_off),
                           ),
-                          border: OutlineInputBorder(borderSide: BorderSide(color: Theme.of(context).focusColor.withOpacity(0.2))),
-                          focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Theme.of(context).focusColor.withOpacity(0.5))),
-                          enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Theme.of(context).focusColor.withOpacity(0.2))),
+                          border: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: Theme.of(context)
+                                      .focusColor
+                                      .withOpacity(0.2))),
+                          focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: Theme.of(context)
+                                      .focusColor
+                                      .withOpacity(0.5))),
+                          enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: Theme.of(context)
+                                      .focusColor
+                                      .withOpacity(0.2))),
                         ),
                       ),
                       SizedBox(height: 30),
                       BlockButtonWidget(
                         text: Text(
                           S.of(context).register,
-                          style: TextStyle(color: Theme.of(context).primaryColor),
+                          style:
+                              TextStyle(color: Theme.of(context).primaryColor),
                         ),
                         color: Theme.of(context).accentColor,
                         onPressed: () {
